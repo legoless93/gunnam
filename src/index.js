a971f876bb358fa91360107b8a36c1321d6c3880
legoless93 <legoless93@yahoo.co.kr>
2017-09-11 13:51:56 +0100

    It has begun

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {Provider} from 'react-redux';
+import {applyMiddleware, createStore} from 'redux';
+import reducers from './.reducers/reducers.jsx';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
+import Home from './components/pages/home.jsx';
 import registerServiceWorker from './registerServiceWorker';
+
+const middleware = applyMiddleware(thunk);
+const store = createStore(reducers, middleware);
+
+const Routes = (
+    <Provider store={store}>
+        <Router history={browserHistory}>
+            <Route path="/" component={App}>
+                <IndexRoute component={Home}/>
+{/*                <Route path="/about" component={About}/> */}
+            </Route>
+        </Router>
+    </Provider>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
